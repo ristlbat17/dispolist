0147a5b12b012eb46ae617ae0df4db5913a1853b
Andreas <andreas.hasenfratz@hotmail.de>
2019-05-13 17:29:59 +0200

    Servants Overview - Company dropdown

--- Ristlbat17.Disposition/ClientApp/src/app/servants/servant-overview.component.ts
+++ Ristlbat17.Disposition/ClientApp/src/app/servants/servant-overview.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  InventoryClient,
+  Company,
+  MaterialInventoryItem,
+  Material,
+  MaterialAllocation,
+  IMaterialInventoryItem
+} from '../services/api_client_generated';
 
 @Component({
     selector: 'servants-overview',
-    template: `
-    <br/>
-    <h2> Befehlsgebungsrapport im Gange ...</h2>
-    `
+    templateUrl: './servant-overview.component.html'
 })
 export class ServantOverviewComponent implements OnInit {
 
-    ngOnInit(): void {
+    companies: Company[];
+    displayCompanyNames: string[];
+    selectedCompany: string;
+    constructor(private inventoryClient: InventoryClient) { }
 
-    }
+    async ngOnInit(): Promise<void> {
+        await this.inventoryClient.getCompanies().subscribe(result => {
+          this.companies = result;
+          this.displayCompanyNames = this.companies.map(c => c.name);
+          this.displayCompanyNames.push('Alle');
+        });
+      }
 
 }
